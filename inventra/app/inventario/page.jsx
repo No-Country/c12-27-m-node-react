e1a0e93492aae4d3f3726797aef8fa3d3254bcfb
Orlandoseoanes <oseoanesdeveloper@gmail.com>
2023-07-07 16:21:37 -0500

    se removio un margin en layout,se empezo a realizar el inventario

--- inventra/app/inventario/page.jsx
+++ inventra/app/inventario/page.jsx
@@ -1,5 +1,28 @@
+import {AiOutlineSearch} from 'react-icons/ai'
+import {IoMdNotifications} from 'react-icons/io'
 export default function Inventario() {
     return (
-        <h1>soy el Inventario</h1>
+        <div className="">
+            <header className="flex justify-around h-20 border-b border-gray-200">
+                <div className="join w-3/4 p-4 justify-start">
+                <input className="input  w-full    input-bordered join-item" placeholder="Buscar"/>
+                <button className="btn join-item   bg-blue-700"> <AiOutlineSearch className="mr-3" size={25}/>   </button>
+                </div>
+
+        
+                <div className="flex     items-center gap-5 p-10">
+                    <IoMdNotifications className="mr-3" size={28} />
+                    <img src='/epyon2.jpg' alt="avatar" className="rounded-full w-[50px] h-[50px]" />
+                    <div>
+                    <p className="text-gray-500">
+                        Nombre de usuario
+                    </p>
+                    <p className="text-gray-400">
+                        Role
+                    </p>
+                    </div>
+                </div>
+            </header>
+      </div>
     )
 }
