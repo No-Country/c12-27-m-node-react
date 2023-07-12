503463717acad7c8e2bce9f704115ce2bac929ae
Pedraza Joaquin <joaquin.n.pedraza@gmail.com>
2023-07-12 13:51:06 -0300

    adding screen add item

--- inventra/app/[home]/login/page.jsx
+++ inventra/app/[home]/login/page.jsx
@@ -3,17 +3,29 @@ import Link from "next/link";
 
 export default function Login() {
     return (
-        <>
+        <div className="flex row">
             <Image
                 src="/../public/portapapeles-explotacion-hombre-tiro-medio.jpg"
                 width={500}
                 height={500}
                 alt="Picture of the author"
             />
-            <h1>soy el login</h1>
-            <Link href='/home/login'> login </Link>
-            <Link href='/home/createAcount'> createAcount </Link>
-            <Link href='/src/routes/dashboard'> Acceder </Link>
-        </>
+            <div className="flex column">
+                <img className="" src='/logo-inventra-blanco.png' alt="avatar" />
+                <h1>soy el login</h1>
+                <div>
+                    <input type="text" />
+                    <input type="password" />
+                    <Link href='/src/routes/dashboard'> Acceder </Link>
+
+                </div>
+                <div>
+                    <small>¿No tienes cuenta?</small>
+                    <Link href='/home/createAcount'> Registrate ahora </Link>
+                    <Link href='/home/login'> login </Link>
+                </div>
+            </div>
+
+        </div>
     )
 }
--- inventra/components/sidebar/Sidebar.jsx
+++ inventra/components/sidebar/Sidebar.jsx
@@ -5,17 +5,17 @@ function Sidebar() {
   return (
     <div className="w-64 bg-[#2969E3] h-full py-10 px-9">
       <div className="py-4 text-gray-500">
-          <img className="" src='/logo-inventra-blanco.png' alt="avatar" />
+        <img className="" src='/logo-inventra-blanco.png' alt="avatar" />
       </div>
       <nav className="text-white text-base font-semibold mt-10">
         <ul className="space-y-2">
-          { links.map(({ label, route, icon: Icon }) => (
+          {links.map(({ label, route, icon: Icon }) => (
             <li key={route}>
               <Link legacyBehavior href={route}>
                 <a className="flex items-center text-white hover:bg-[#003EBB] hover:text-white py-3 px-4 rounded-lg font-medium">
                   <Icon className="inline-block mr-3" size={28} />
                   {label}
-                </a>  
+                </a>
               </Link>
             </li>
           ))}
